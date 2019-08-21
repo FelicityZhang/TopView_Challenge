f5fdfcf762e26adc2e51c21b23581c2a248b6813
felicityzhang <xz1574@nyu.edu>
2019-08-21 11:42:31 -0400

    add data

---
+++ index.js
@@ -0,0 +1,5 @@
+const imageDiv = document.querySelector( ".image" )
+const priceDiv = document.querySelector( ".price" )
+const nameDiv = document.querySelector( ".name" )
+const typeDiv = document.querySelector( ".type" )
+
